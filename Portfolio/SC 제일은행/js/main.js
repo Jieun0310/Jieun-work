8a253c26ec87c2e3e2e53f3f16bc7ecb632040d3
cmkm3010 <cmkm8384@gmail.com>
2023-11-10 17:07:54 +0900

    init

--- Portfolio/SC 제일은행/js/main.js
+++ Portfolio/SC 제일은행/js/main.js
@@ -6,4 +6,33 @@ $(function(){
             $(this).css('left', 0)
     })},4000)
 
+    // 메뉴
+    $('.sub_gnb').hover(
+        function(){
+            $(this).find('ul').stop().slideDown()
+        },
+        function(){
+            $(this).find('ul').stop().slideUp()
+        },
+    )
+
+    // card page slide
+     var slide = $('.slide')
+    var current = 0
+
+    slide.eq(current).show()
+
+    setInterval(function(){
+        var next = (current + 1) % slide.length
+
+        slide.eq(current).fadeOut()
+        slide.eq(next).fadeIn()
+        current = next
+    },4000)
+    
 })
+
+
+
+
+
